2858bbca6b602f703934bb7822dec48bc937da27
ahaval <haval.amol@gmail.com>
2016-08-23 20:26:53 -0700

    Add files via upload

---
+++ Kafka/src/main/java/Kafka_Sample_Producer/Kafka/App.java
@@ -0,0 +1,34 @@
+package Kafka_Sample_Producer.Kafka;
+
+import java.util.Properties;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+/**
+ * Hello world!
+ *
+ */
+public class App  
+{
+	private static Producer<Integer, String> producer;
+    private final Properties properties = new Properties();
+
+    public App() {
+        properties.put("metadata.broker.list", "52.4.184.235:6667");
+        properties.put("serializer.class", "kafka.serializer.StringEncoder");
+        properties.put("request.required.acks", "1");
+        producer = new Producer<>(new ProducerConfig(properties));
+    }
+
+    public static void main(String[] args) {
+        new App();
+        String topic = "test";
+        String msg = "Hello from Java";
+        System.out.println(" Sending message ");
+        KeyedMessage<Integer, String> data = new KeyedMessage<>(topic, msg);
+        producer.send(data);
+        System.out.println(" Sent message ");
+        producer.close();
+    }
+}
